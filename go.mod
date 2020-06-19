24cdcf388632945e6dbe09c08224bf90c18bc46f
8luebottle <itbiz.irs@gmail.com>
2020-06-19 21:27:36 +0900

    WIP : db setup

--- go.mod
+++ go.mod
@@ -6,5 +6,8 @@ require (
 	github.com/google/uuid v1.1.1
 	github.com/jinzhu/gorm v1.9.12
 	github.com/labstack/echo/v4 v4.1.16
+	github.com/lib/pq v1.7.0 // indirect
 	github.com/pkg/errors v0.9.1
+	github.com/spf13/viper v1.7.0 // indirect
+	gopkg.in/yaml.v2 v2.2.4
 )
--- server/base.go
+++ server/base.go
@@ -2,22 +2,53 @@ package server
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
 
 	"github.com/8luebottle/Wells-Far-Go/api"
+	"github.com/8luebottle/Wells-Far-Go/config"
 	mw "github.com/8luebottle/Wells-Far-Go/middleware"
+	"github.com/8luebottle/Wells-Far-Go/model"
 )
 
 type Server struct {
 	Database *gorm.DB
+	Conf     config.Config
 }
+
+var d *gorm.DB
 
 func Run() {
 	e := NewEcho()
-	fmt.Print(api.BANNER)
+	apiPath := os.Getenv("WELLS_FAR_GO_CONFIG")
+	if err := config.InitAPIConfig(apiPath); err != nil {
+		log.Fatalf("init api config : '%v'", err)
+	}
+
+	db := NewDB()
+	// Todo : Create Table
+	if err := createTable(db); err != nil {
+		log.Fatalf("create database table : '%v'", err)
+	}
+
 	e.Logger.Fatal(e.Start(api.PORT))
+}
+
+func (s *Server) createTable() error {
+	db := NewDB
+	if err := s.Database.AutoMigrate(
+		&model.Account{}, &model.Address{},
+		&model.Bank{},
+		&model.Customer{},
+		&model.Transaction{},
+	).Error; err != nil {
+		return errors.Wrap(err, "auto migrate database table")
+	}
+	return nil
 }
 
 func NewEcho() *echo.Echo {
@@ -25,4 +56,19 @@ func NewEcho() *echo.Echo {
 	InitRouters(e)
 	mw.EnableCORS(e)
 	return e
+}
+
+func NewDB() *gorm.DB {
+	con := config.GetConfig()
+	dbInfo := con.ConnectDB()
+	db, err := gorm.Open(con.DB.Driver, dbInfo)
+	if err != nil {
+		fmt.Sprintf("err : %s", err)
+	}
+	db.LogMode(true)
+	return db
+}
+
+func Conn() *gorm.DB {
+	return d
 }
