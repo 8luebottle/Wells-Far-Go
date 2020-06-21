fb01a0799b9973f13c5e85b85ae44660844d04d0
I Re Shin <48475824+8luebottle@users.noreply.github.com>
2020-06-21 16:07:30 +0900

    🌱 Add swift code (#8)
    Swift Code (8-11 characters)
    - Bank Code (4) | required
    - Country Code (2) | required
    - Location Code (2) | required
    - Branch Code (3) | optional

--- server/route.go
+++ server/route.go
@@ -2,8 +2,22 @@ package server
 
 import (
 	"github.com/labstack/echo/v4"
+
+	"github.com/8luebottle/Wells-Far-Go/api"
+	"github.com/8luebottle/Wells-Far-Go/handler"
+	"github.com/8luebottle/Wells-Far-Go/repository"
+	"github.com/8luebottle/Wells-Far-Go/service"
 )
 
 func InitRouters(e *echo.Echo) {
-	//v := e.Group(api.VERSION)
+	v := e.Group(api.VERSION)
+
+	bankRepo := repository.ParseBankStorer()
+	bankSvc := service.ParseBankServer(bankRepo)
+	bankHandler := handler.ParseBankController(bankSvc)
+	vBank := v.Group("/banks")
+	{
+		// Todo : Admin Auth
+		vBank.POST("/new", bankHandler.CreateBank)
+	}
 }
